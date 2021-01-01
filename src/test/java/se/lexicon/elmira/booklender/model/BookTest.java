ca078b0f2afecb5fc64d9402872f3224c30a5c6e
Elmira Madadi <elmiramadadi@gmail.com>
2021-01-01 10:22:00 +0100

    Book created and added unit test.

---
+++ src/test/java/se/lexicon/elmira/booklender/model/BookTest.java
@@ -0,0 +1,55 @@
+package se.lexicon.elmira.booklender.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    Book testObject;
+    @Test
+    void testEquals() {
+        Book copy = new Book("Dictionary",5, BigDecimal.valueOf(600),"reference source");
+        assertTrue(testObject.equals(copy));
+    }
+
+    @Test
+    void testToString() {
+        String toString = testObject.toString();
+
+        assertTrue(toString.contains(Integer.toString(testObject.getBookId())));
+        assertTrue(toString.contains(testObject.getTitle()));
+        assertTrue(toString.contains(testObject.getDescription()));
+        assertTrue(toString.contains(testObject.getFinePerDay().toString()));
+    }
+
+    @BeforeEach
+    void setUp() {
+        testObject = new Book("Dictionary",5, BigDecimal.valueOf(600),"reference source");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void successfully_created(){
+        assertNotNull(testObject);
+        assertTrue(testObject.getBookId() == 0);
+        assertEquals("Dictionary", testObject.getTitle());
+        assertEquals("reference source", testObject.getDescription());
+        assertEquals(BigDecimal.valueOf(600), testObject.getFinePerDay());
+    }
+
+    @Test
+    void testHashcode() {
+        Book copy = new Book("Dictionary",5, BigDecimal.valueOf(600),"reference source");
+        assertEquals(copy.hashCode(), testObject.hashCode());
+    }
+
+}
