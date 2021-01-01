d9348999d472030856392cb612d7a52f3b456123
Elmira Madadi <elmiramadadi@gmail.com>
2021-01-01 08:12:23 +0100

    Initial commit

---
+++ src/test/java/se/lexicon/elmira/booklender/BooklenderApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.elmira.booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
